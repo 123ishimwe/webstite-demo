8885efbccd6900a4c472699ab8973578aa4e43dc
123ishimwe <christianishimwe12345@gmail.com>
2024-11-28 09:48:52 +0200

    Add files via upload

---
+++ school.java
@@ -0,0 +1,5 @@
+// Simple JavaScript for form validation or any additional features
+document.querySelector('form').addEventListener('submit', function(event) {
+  event.preventDefault();
+  alert('Your message has been sent!');
+});
